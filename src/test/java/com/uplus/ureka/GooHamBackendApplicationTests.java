8795b9162aa4454937f4dc9e01b54c98340aa853
alotofhee <ekgmljeong@gmail.com>
2025-03-14 10:14:10 +0900

    출발

---
+++ src/test/java/com/uplus/ureka/GooHamBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uplus.ureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GooHamBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

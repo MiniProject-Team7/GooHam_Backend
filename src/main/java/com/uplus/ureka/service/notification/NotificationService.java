14d69ab9eeaee136a3cb90d3627e9fc42aae8234
alotofhee <ekgmljeong@gmail.com>
2025-03-20 15:01:47 +0900

    FEAT: 기간 지난 알림 삭제 스케줄러 구현

--- src/main/java/com/uplus/ureka/service/notification/NotificationService.java
+++ src/main/java/com/uplus/ureka/service/notification/NotificationService.java
@@ -6,6 +6,9 @@ import com.uplus.ureka.dto.notification.NotificationResponseDTO;
 import com.uplus.ureka.exception.CustomExceptions;
 import com.uplus.ureka.repository.notification.NotificationMapper;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -16,6 +19,9 @@ import java.util.List;
 @Transactional
 public class NotificationService {
     private final NotificationMapper notificationMapper;
+
+    private static final Logger logger = LoggerFactory.getLogger(NotificationService.class);
+
 
     // ✅ 게시글 존재 여부 체크
     public boolean checkPostExists(Long postId) {
@@ -87,5 +93,27 @@ public class NotificationService {
     // ✅ 알림 상태 업데이트
     public void markNotificationAsRead(Long notificationId, Long userId) {
         notificationMapper.updateNotification(notificationId, userId);
+    }
+
+    @Scheduled(cron = "0 19 14 * * ?") // TEST
+    // ✅ 일정 시간이 지난 알림 삭제
+    public void deleteOldNotifications() {
+        // 기한이 지난 스케줄 조회
+        List<Long> pastNotifications = notificationMapper.findPastNotifications();
+
+        // 기한이 지난 스케줄이 없을 경우 예외 처리
+        if (pastNotifications == null || pastNotifications.isEmpty()) {
+            logger.warn("기한이 지난 참여 신청 내역이 없습니다.");
+            throw new CustomExceptions("기한이 지난 참여 신청 내역이 없습니다.");
+            // return   // 기한이 지난 스케줄이 없으면 작업 종료
+        }
+
+        // 기한이 지난 신청 내역 삭제
+        try {
+            notificationMapper.deleteOldNotifications(pastNotifications);
+            logger.info("기한이 지난 알림이 삭제되었습니다.");
+        } catch (Exception e) {
+            throw new CustomExceptions("알림 삭제 중 오류 발생");
+        }
     }
 }
